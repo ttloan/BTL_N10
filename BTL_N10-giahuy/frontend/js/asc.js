2b77ac58a9f6caa8704a061700087fc3c425b5ea
your name <100056958+phamgiahuy62th2@users.noreply.github.com>
2022-04-11 10:51:57 +0700

    giahuy

--- BTL_N10-giahuy/frontend/js/asc.js
+++
@@ -1,9 +0,0 @@
-function generateColorforShop =()=>{
-    let color;
-    let r=Math.floor(Math.random()*256);
-    let g=Math.floor(Math.random()*256);
-    let b=Math.floor(Math.random()*256);
-    color=`rgb(${r} ,${g} ,${b})`
-    dropdown-toggle.style.color=color
-}
-setInterval(generateColorforShop,1000)
